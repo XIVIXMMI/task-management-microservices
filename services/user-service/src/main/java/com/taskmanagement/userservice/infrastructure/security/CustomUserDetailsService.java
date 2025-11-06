42b9976046f0fadc0f6334a0ad109432fa7fd2ef
XIVIXMMI <lethaikhoinguyen@gmail.com>
2025-11-06 19:19:37 +0700

    add: create custom UserDetails and UserDetailsService for spring security

---
+++ services/user-service/src/main/java/com/taskmanagement/userservice/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.taskmanagement.userservice.infrastructure.security;
+
+import com.taskmanagement.userservice.domain.entity.User;
+import com.taskmanagement.userservice.domain.exception.UserNotFoundException;
+import com.taskmanagement.userservice.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * CustomUserDetailService load user from Database
+ * Spring Security invoked this service when authenticate
+ */
+@Service
+@RequiredArgsConstructor
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * When user login or validate JWT
+     * Spring Security invoked this method with username (email)
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException(
+                        "User not found with email " + email
+                ));
+        return CustomUserDetails.fromUser(user);
+    }
+}
